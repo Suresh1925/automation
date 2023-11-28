578c4eb0e243435f3d268f721bb0ba0f6ea656a3
Suresh1925 <jerry25suresh@gmail.com>
2023-11-28 19:09:09 +0530

    cmd parameters

--- Suresh_Task/ListOfLeapYears.java
+++ Suresh_Task/ListOfLeapYears.java
@@ -3,11 +3,13 @@ package automation.Suresh_Task;
 import java.time.Year;
 
 public class ListOfLeapYears {
-    public static void main(String[] args) {
-            int first =Integer.parseInt(args[0]);
-            int last=Integer.parseInt(args[1]);
-            for (int year = first; year <= last; year++) {
-                if (Year.of(year).isLeap()) {
+    public static void main(int[] args) {
+            int first =args[0];
+            int last=args[1];
+            for (int year = first; year <= last; year++)
+            {
+                if (Year.of(year).isLeap()) 
+                {
                     System.out.println(year);
                 }
             }
