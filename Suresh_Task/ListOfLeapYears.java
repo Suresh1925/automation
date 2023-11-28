5a6a68f0158237ae9570044dbf93c0732abba6eb
Suresh1925 <jerry25suresh@gmail.com>
2023-11-28 13:38:58 +0530

    Leapyear updated

--- Suresh_Task/ListOfLeapYears.java
+++ Suresh_Task/ListOfLeapYears.java
@@ -2,7 +2,7 @@ package automation.Suresh_Task;
 
 public class ListOfLeapYears {
     public static void main(String[] args) {
-    for (int i = 1800; i < 1900; i++) {
+    for (int i = 2000; i < 2025; i++) {
         if ((i % 4 == 0 && i % 100 != 0) || i % 400 == 0) {
             System.out.printf("%4d%n", i);
         }
