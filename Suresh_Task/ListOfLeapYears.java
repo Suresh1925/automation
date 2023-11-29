2c3f8515e63355c67313f046b7771596320589f1
Suresh1925 <jerry25suresh@gmail.com>
2023-11-29 15:39:43 +0530

    HTML Files added

--- Suresh_Task/ListOfLeapYears.java
+++ Suresh_Task/ListOfLeapYears.java
@@ -3,7 +3,9 @@ package automation.Suresh_Task;
 import java.time.Year;
 
 public class ListOfLeapYears {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        System.out.println(args[0].getClass());
             int first =Integer.parseInt(args[0]);
             int last=Integer.parseInt(args[1]);
             for (int year = first; year <= last; year++)
